82c133ddbb37a2206d4577f3d95833c2ca9707d8
Igor Arecippo <iarecippo@gmail.com>
2023-06-15 11:59:03 -0300

    lint fix

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -4,7 +4,7 @@ from ting_file_management.priority_queue import PriorityQueue
 
 def test_basic_priority_queueing():
     priority_queue = PriorityQueue()
-      # Test enqueue with priority
+    # Test enqueue with priority
     file1 = {
         "nome_do_arquivo": "arquivo1.txt",
         "qtd_linhas": 4,
@@ -13,7 +13,10 @@ def test_basic_priority_queueing():
     file2 = {
         "nome_do_arquivo": "arquivo2.txt",
         "qtd_linhas": 7,
-        "linhas_do_arquivo": ["linha1", "linha2", "linha3", "linha4", "linha5", "linha6", "linha7"],
+        "linhas_do_arquivo": ["linha1",
+                              "linha2",
+                              "linha3",
+                              "linha4", "linha5", "linha6", "linha7"],
     }
     file3 = {
         "nome_do_arquivo": "arquivo3.txt",
@@ -32,4 +35,3 @@ def test_basic_priority_queueing():
     assert priority_queue.high_priority.dequeue() == file3
     assert priority_queue.regular_priority.dequeue() == file1
     assert priority_queue.regular_priority.dequeue() == file2
-   
