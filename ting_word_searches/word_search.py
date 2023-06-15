14c429d89599955c300d692446c1d6103ffc2b51
Igor Arecippo <iarecippo@gmail.com>
2023-06-15 12:35:19 -0300

    feat: func existis_word incomplete

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for file_data in instance.queue:
+        file_name = file_data["nome_do_arquivo"]
+        lines = file_data["linhas_do_arquivo"]
+
+        occurrences = []
+        for i, line in enumerate(lines, start=1):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": i, "conteudo": line})
+
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": occurrences
+            })
+
+    return result
 
 
 def search_by_word(word, instance):
